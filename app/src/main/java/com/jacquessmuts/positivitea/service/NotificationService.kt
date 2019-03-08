44889965687ca0648f9bb69d8233fbce8a23f4da
Jacques Chester Smuts <chester@jacquessmuts.com>
2019-03-08 19:40:00 +0800

    Notification is working. Next is to schedule it.

--- app/src/main/java/com/jacquessmuts/positivitea/service/NotificationService.kt
+++ app/src/main/java/com/jacquessmuts/positivitea/service/NotificationService.kt
@@ -1,8 +1,12 @@
 package com.jacquessmuts.positivitea.service
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat.getSystemService
 import com.jacquessmuts.positivitea.R
 import com.jacquessmuts.positivitea.database.TeaDb
 import com.jacquessmuts.positivitea.model.TeaPreferences
@@ -33,6 +37,28 @@ class NotificationService(private val context: Context,
     private val rxSubs: CompositeDisposable by lazy { CompositeDisposable() }
 
     var teaPreferences: TeaPreferences = TeaPreferences()
+
+    init {
+        //TODO: do this slightly less often
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = context.getString(R.string.notification_channel_name)
+            val descriptionText = context.getString(R.string.notification_channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
 
     fun showRandomNotification() {
         launch {
